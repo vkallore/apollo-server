16e1f93838c3e8a13aa1ed2deeba0e47fef593f4
Vaishak Kallore <vaikoovery@gmail.com>
2019-02-19 15:45:36 +0530

    1. Used ES6/ES2015 in node.js 2. Added minimum requirement of node & npm versions & made it strict

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-const app = require('express')()
-const { ApolloServer } = require('apollo-server-express')
-const { typeDefs, resolvers } = require('./schema')
+import express from 'express'
+import http from 'http'
+
+import { ApolloServer } from 'apollo-server-express'
+import { typeDefs, resolvers } from './schema'
+
+const app = express()
+
+const server = http.createServer(app)
 
 // In the most basic sense, the ApolloServer can be started
 // by passing type definitions (typeDefs) and the resolvers
 // responsible for fetching the data for those types.
-const server = new ApolloServer({ typeDefs, resolvers })
+const apollo = new ApolloServer({ typeDefs, resolvers })
 
-server.applyMiddleware({ app }) // app is from an existing express app
+apollo.applyMiddleware({ app }) // app is from an existing express app
 
 // This `listen` method launches a web-server.  Existing apps
 // can utilize middleware options, which we'll discuss later.
 app.listen({ port: 4000 }, () => {
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  console.log(`🚀 Server ready at http://localhost:4000${apollo.graphqlPath}`)
 })
